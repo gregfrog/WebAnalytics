24e21aaaa79374b2ef9f9ea2d421ff3cfa7d51d8
Greg <greg@firmansyah.com>
2025-07-27 19:54:27 +1000

    improve rscript detection, remove bash dependency

--- inst/templates/makerpt.sh
+++ inst/templates/makerpt.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ -e $1 ]]; then
+if [ -e "$1"]; then
 configfile=$1
 nameStem=`basename $1`
-elif [[ -e $1.config ]]; then
+elif [ -e "$1.config" ]; then
     configfile=$1.config
     nameStem=$1
 else
@@ -22,40 +22,56 @@ options(brew.extended.error = TRUE)
 options(show.error.messages = TRUE)
 a = brew::brew(file="sampleRfile.R", output="$texFileNameStem.tex",envir=e)
 if(exists("a")) {
-if(class(a) == "try-error"){
-	traceback(0)
-	print(a)
-	quit(save="no",status=10)
-}
-print("normal end of R code")
+  if(class(a) == "try-error"){
+  	traceback(0)
+  	print(a)
+  	quit(save="no",status=10)
+  }
+  print("normal end of R code")
 }
 quit(save="no",status=0)
 
 End-of-script
 
-if [[ -n "${R_HOME}"  ]]; then 
+if [ -n "${R_HOME}"  ]; then 
 	"${R_HOME}/bin/"Rscript $brewFileName
 else
-	Rscript $brewFileName
+  which Rscript
+  rc=$?
+
+  if [ $rc -eq 0 ]; then
+  
+  	Rscript $brewFileName
+
+    rc=$?
+
+  else
+    echo "Rscript not found on path, set R_HOME to the directory whwere R is installed, that is the directory ABOVE bin/Rscript"
+    exit $rc
+  fi
+fi
+
+if [ $rc -ne 0 ]; then 
+  echo "r script failed"
+  exit $rc
 fi 
 
+xelatex "$texFileNameStem.tex"
+xelatex "$texFileNameStem.tex"
+xelatex "$texFileNameStem.tex"
 
 rc=$?
 
 if [ $rc -eq 0 ]; then
-		xelatex "$texFileNameStem.tex"
-		xelatex "$texFileNameStem.tex"
-		xelatex "$texFileNameStem.tex"
-
-		rc=$?
-		if [ $rc -eq 0 ]; then
-			rm $brewFileName
-			rm $texFileNameStem.out
-			rm $texFileNameStem.log
-			rm $texFileNameStem.toc
-			rm $texFileNameStem.aux
-			rm $texFileNameStem.tex
-		fi 
+	rm $brewFileName
+	rm $texFileNameStem.out
+	rm $texFileNameStem.log
+	rm $texFileNameStem.toc
+	rm $texFileNameStem.aux
+	rm $texFileNameStem.tex
 else
-  echo "r script failed"
-fi
+  thisdir=`pwd`
+  echo "xelatex failed to process the generated tex file $texFileNameStem.tex, file kept in $thisdir"
+  exit $rc
+fi 
+
